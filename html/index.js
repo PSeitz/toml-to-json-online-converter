25dbdbe0b561cfa5384930a5361aee0ce55014b1
Pascal Seitz <pascal.seitz@sap.com>
2019-09-18 16:58:14 +0200

    add website

--- html/index.js
+++ html/index.js
@@ -6,16 +6,17 @@ document.querySelector('#to_json').addEventListener('click', convertToJson)
 document.querySelector('#to_toml').addEventListener('click', convertToToml)
 
 function convertToToml(){
-    try{
-        document.getElementById("output").value = wasm.convert_json_to_toml(document.getElementById("input").value);
-    }catch(e){
-        document.getElementById("output").value = e.toString()
-    }
+    convert(wasm.convert_json_to_toml)
 }
 
 function convertToJson(){
+    convert(wasm.convert_toml_to_json)
+}
+
+
+function convert(fun){
     try{
-        document.getElementById("output").value = wasm.convert_toml_to_json(document.getElementById("input").value);
+        document.getElementById("output").value = fun(document.getElementById("input").value);
     }catch(e){
         document.getElementById("output").value = e.toString()
     }
